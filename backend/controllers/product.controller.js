34d1d7f3f2acbadd03016fa94196ccc8dee5cec7
Kaan Koç <kaankoc@Kaan-MacBook-Air.local>
2025-06-02 09:44:25 +0300

    deploy surunu düzenleme

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -98,27 +98,6 @@ export const getProductsByCategory = async (req, res) => {
   }
 };
 
-// Kullanıcı - Rastgele önerilen ürünler
-/* export const getRecommendedProducts = async (req, res) => {
-  try {
-    const products = await Product.aggregate([
-      { $sample: { size: 4 } },
-      {
-        $project: {
-          _id: 1,
-          name: 1,
-          description: 1,
-          image: 1,
-          price: 1,
-        },
-      },
-    ]);
-    res.json(products);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-*/
 
 
 // Admin - Öne çıkarma durumu değiştir
