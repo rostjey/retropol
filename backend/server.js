f7dc5ad7a7c3e62d0d0d7d0f48b4bbed049928c3
Kaan Koç <kaankoc@Kaan-MacBook-Air.local>
2025-06-03 12:50:18 +0300

    patch eklendi

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cors({
   origin: "https://retropol-ruddy.vercel.app", // Frontend URL
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"] // İzin verilen metodlar
+  methods: ["GET", "POST", "PUT", "DELETE","PATCH"] // İzin verilen metodlar
 }));
 
 app.use(cookieParser());
