93b5a4a26cb0b5633c5eb6e6d8980ed3f514bfa1
Kaan Koç <kaankoc@Kaan-MacBook-Air.local>
2025-06-04 12:44:34 +0300

    fafaf

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -24,7 +24,7 @@ const setCookies = (res, accessToken, refreshToken) => {
     httpOnly: true,
     secure: true,                // sadece https (Render'da zorunlu)
     sameSite: "none",            // cross-site cookie
-    domain: 'retropol-ruddy.vercel.app',// Tam domain vercel için gerekli
+    //domain: 'retropol-ruddy.vercel.app',// Tam domain vercel için gerekli
     maxAge: 15 * 60 * 1000,
     //proxy: isProduction // 👈 Render için kritik
   });
@@ -33,7 +33,7 @@ const setCookies = (res, accessToken, refreshToken) => {
     httpOnly: true,
     secure: true,
     sameSite: "none",
-    domain: 'retropol-ruddy.vercel.app', // Tam domain vercel için gerekli
+    //domain: 'retropol-ruddy.vercel.app', // Tam domain vercel için gerekli
     maxAge: 7 * 24 * 60 * 60 * 1000,
     //proxy: isProduction // 👈 Render için kritik
   });
@@ -122,7 +122,7 @@ export const refreshToken = async (req, res) => {
       httpOnly: true,
       secure: true,
       sameSite: "none",
-      domain: 'retropol-ruddy.vercel.app', // Tam domain vercel için gerekli
+      //domain: 'retropol-ruddy.vercel.app', // Tam domain vercel için gerekli
       maxAge: 15 * 60 * 1000,
       //proxy: isProduction // 👈 Render için kritik
     });
