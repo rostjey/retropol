1a1bf37f32164756ec28531bfcd0cc76bfc105d0
Kaan Koç <kaankoc@Kaan-MacBook-Air.local>
2025-06-05 12:01:24 +0300

    tailwindconfig

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,10 @@
 // tailwind.config.js
 module.exports = {
     content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
-      "./app/**/*.{js,ts,jsx,tsx}", // varsa
+        "./pages/**/*.{js,ts,jsx,tsx}", // varsa
+        "./components/**/*.{js,ts,jsx,tsx}", // varsa
+        "./app/**/*.{js,ts,jsx,tsx}", // varsa
+        "./src/**/*.{js,ts,jsx,tsx}", // varsa
     ],
     theme: {
       extend: {
@@ -16,7 +17,7 @@ module.exports = {
               boxShadow: '0 0 0px rgba(255, 165, 0, 0)',
             },
             '50%': {
-              boxShadow: '0 0 25px rgba(255, 165, 0, 0.5)',
+              boxShadow: '0 0 100px rgba(255, 165, 0, 1)',
             },
           },
         },
